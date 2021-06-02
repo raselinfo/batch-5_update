6ed21c77c939ebb3918408d0ea74f95b3b5c7031
raselinfo <raselinfo52@gmail.com>
2021-06-02 22:18:11 +0600

    class97

---
+++ Class 97  (Javascript)/index.js
@@ -0,0 +1,97 @@
+// this program will print 1 to 10 sum
+
+
+// let sum=0;
+// for(let i=0;i<=10;i+=1){   
+//     sum += i
+//     console.log(`${sum} + ${i} = ${sum}`)
+
+// }
+
+// console.log("Result = "+sum)
+
+
+// let i=1;
+// while(i<=10){
+//     console.log(i)
+//     i++;
+// }
+
+// let i=0;
+// while(i<=100){
+//     if(i%2===0){
+//         if(i!==0){
+//             console.log(i + " is Even")
+//         }
+//     }
+//     i+=1;
+// }
+
+// let isRunning=true;
+
+// while (isRunning){
+//     let rend = Math.floor(Math.random() * 10 + 1);
+//     if(rend===9){
+//         console.log("winner winner chicken dinner")
+//         isRunning=false;
+//     }else{
+//         console.log("you have got "+ rend)
+//     }
+// }
+
+// const rend=Math.floor(Math.random()*5+1)
+// console.log(rend)
+
+// let i=1;
+// while(i<=100){
+//     if(i===5){
+
+//     }else{
+//         console.log(i)
+//     }  
+
+//     i++;
+// }
+
+
+// let isRunning=true;
+// while(isRunning){
+//     let i=1;
+//     while(i<=100){
+//         if(i===55){
+//             isRunning=false;
+//             i=99;
+//         }else{
+//             console.log(i)
+//         }
+//         i++;
+//     }
+// }
+
+
+// let i=0;
+// let sum=0;
+// while(i<=10){
+   
+//     console.log(`${sum} + ${i} = ${sum += i}`)
+
+//     i++;
+// }
+
+
+// Do While loop
+
+// let i=1;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i==0)
+
+// while(condtion){
+
+// }
+
+// let isRunning=false;
+// do{
+//     console.log("I am first running")
+// }while(isRunning)
