9a7d6e17404feb189555a4844435db3cd08d2aa9
raselinfo <raselinfo52@gmail.com>
2021-06-07 23:29:14 +0600

    Javascript class 98

---
+++ Class 98  (Javascript)/index.js
@@ -0,0 +1,100 @@
+// for loop
+
+// for(let i=0;i<=10;i++){
+//     console.log(i)
+// }
+
+// while loop
+
+// let i=0;
+// while(i<=10){
+//     console.log(i)
+
+
+//     i++;
+// }
+
+
+// let i=0;
+// do{
+//     console.log(i)
+
+//     i++
+// }while(i<=10)
+
+
+
+
+// loop
+// i=1
+// condition:6;
+// i++
+
+// for(let i=1;i<=6;i++){
+//     let result=""
+//    for(let j=1;j<=i;j++){
+//     // result=result+j;
+//     // result=`${result +" "+ j}`
+//     result+=j; // result=result+j;
+//    }
+//    console.log(result)
+// }
+
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+// 1 2 3 4 5 6
+
+// A
+// A A
+// A A A
+// A A A A
+// A A A A A
+// A A A A A A
+// A A A A A A A
+
+// for(let i=1;i<=7;i++){
+//     let result=""
+//     for(let j=1;j<=i;j++){
+//         result+="A"; //result=result+"A" = "A"
+//     }
+//     console.log(result)
+// }
+
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+// * * * * * *
+// * * * * * * *
+
+// #
+// # #
+// # # #
+// # # # #
+// # # # # #
+
+// i=1;
+// last value :6
+
+// for(let i=1;i<=6;i++){
+//     let result=""
+//     for(let j=1;j<=6;j++){
+//         result+="* "
+//     }
+//     console.log(result)
+// }
+
+// let n=prompt("Enter a number")
+
+
+// for(let i=1;i<=Number(n);i++){
+//     let result=""
+//     for(var j=1;j<=i;j++){
+//         result+=j+" ";
+//     }
+//     console.log(result)
+// }
