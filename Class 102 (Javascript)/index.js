3e6db0fbb53c21720a05efaf16325c093da5e9c6
Rasel Hossain <raselinfo52@gmail.com>
2021-06-23 22:45:18 +0600

    class 103

---
+++ Class 102 (Javascript)/index.js
@@ -0,0 +1,43 @@
+// console.log("a" < "A")
+// console.log("a" > "z")
+// console.log("A" >"Z")
+// console.log("Z" > "z")
+
+// console.log("abz" < "bcd") 
+// small,small,big
+// big,big,small
+// console.log("001"==1)
+
+// console.log("Rasel"+" "+"Hossain")
+// console.log("rasel".concat("hossain"))
+
+// console.log("Rasel".substr(4))
+// let name ="Lorem ipsum dolor sit amet consectetur adipiscing elit, tincidunt pretium potenti ut penatibus praesent, est ante gravida nisi commodo malesuada."
+
+// console.log(name.substr(0,6))
+// if (name.substr(0, 6) ==="Bangla"){
+//     console.log("I love Bangladesh")
+// }
+// let sotrChar = ""
+// let input="n"
+// for(let i=0;i<=name.length;i++){
+  
+//     if (name.charAt(i) === input){
+//         sotrChar=sotrChar+name.charAt(i)
+      
+//     }    
+// }
+// console.log(sotrChar.length)
+
+
+// let name="RaselOfficial"
+// let result=name.charAt(name.length-1)
+// console.log(result)
+let text =`          Lorem ipsum dolor sit amet consectetur adipiscing elit, dictumst felis posuere leo est primis, taciti volutpat            luctus dignissim proin magna.          `
+
+// console.log(text.startsWith("L"))
+// console.log(text.endsWith("magna"+"."))
+
+// console.log(text.toUpperCase())
+// console.log(text.toLowerCase());
+console.log(text.trim())
---
+++ Class 103 (Javascript)/index.js
@@ -0,0 +1,39 @@
+// console.log("ZAc" < "zAB")
+
+// console.log("Hello,World".split(","))
+// console.log("Hello and hello and hello and".split(" and "))
+// console.log("Rasel".split(""))
+// let string ="Lorem ipsum dolor sit amet consectetur adipiscing elit scelerisque sollicitudin vivamus, tellus et lectus posuere leo nullam suspendisse arcu tempus."
+
+// let splitString=string.split("")
+// console.log(splitString.length)
+// console.log(splitString[2])
+// let result=""
+// for (let i = 0; i <= splitString.length;i++){
+
+//     if(splitString[i]==="i"){
+//         result = result + splitString[i]
+//     }
+// }
+// console.log(result.length)
+// let stringLength=0;
+// for (let i = 0; i <= splitString.length;i++){
+//     let letter = splitString[i]
+//     if(letter==="s"){
+//         stringLength++;
+//     }
+// }
+// console.log(stringLength)
+
+
+// let str="raselofficial.com"
+// console.log(str.length)
+// let length=0;
+// while(true){
+//     if(str.charAt(length)===""){
+//         break;
+//     }else{
+//         length++
+//     }
+// }
+// console.log(length)
