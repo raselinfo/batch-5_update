80fdefb1f16587fbcfcd297fc6b4c77ad6a9064e
Rasel Hossain <raselinfo52@gmail.com>
2021-06-26 23:06:04 +0600

    class 104

---
+++ Class 104 (Javascript)/index.js
@@ -0,0 +1,42 @@
+// const str1 = 'Cats are the best!';
+
+// console.log(str1.endsWith('best',17));
+// const questions=[
+//     "Are you ready?",
+//     "You are a bad body",
+//     "Are you a girl?",
+//     "i am not a rich man",
+//     "is rich man are good?"
+// ]
+// let qus=0;
+// for(let i=0;i<=(questions.length-1);i++){
+//    let item=questions[i]
+//    console.log(item)
+//     if(item?.endsWith("?")){
+//         qus++;
+//     }
+// }
+// console.log(qus)
+
+// const cars=["BMW","Suzuki","Toyota","TVS"]
+// const arr=[1,"String",true,false,undefined,null,5.55,"5,55"]
+
+// console.log(cars[4])
+// console.log(cars.length)
+
+// const arr = Array(1, "String", true, false, undefined, null, 5.55, "5,55")
+// const arr=[1,2,3,4]
+// arr[1]=true
+// arr[4]=5
+// arr[5]=6
+// arr[6]="Rasel"
+// console.log(arr)
+// Read
+// wright
+// insert
+// update
+// const arr=[]
+// arr[0]="rasel"
+// arr[1]="bijoy"
+// arr[6]="Number six"
+// console.log(arr[2])
