8184e957baab338302462715539dcdc4d60fe1b7
Rasel Hossain <raselinfo52@gmail.com>
2021-07-07 23:08:29 +0600

    class 106

---
+++ Class 106 (Javascript)/index.js
@@ -0,0 +1,30 @@
+
+
+// const arr=[1,2,3,4,5,6,7,8,9]
+// arr[7]="Rasel"
+// arr.push("Rasel")
+// arr.unshift("Nahid")
+// arr.pop()
+// arr.shift()
+// arr.splice(1,5,"Rasel","Santo")
+// console.log(arr);
+
+// const cars=["cars a","cars b","cars c","cars d"]
+// cars.splice(2,0,"Rasel","Nahid")
+
+// console.log(cars)
+
+
+// const arr=[1,2,3,4,5,6,7,8,9]
+// arr.splice(2,0,"a","b")
+// console.log(arr)
+
+// const cars=["cars a","cars b","cars c","cars d"]
+// cars.splice(2,0,"a","b")
+// const result=cars.slice(0,2)
+
+// console.log(cars)
+// console.log(result)
+
+// let userData=2;
+// const arr=[1,2,3,4,5,6,"a","z","c"]
