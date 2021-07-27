d0e3aa0cbb23c889a0e5ffa7ef626b6b986f54d6
Rasel Hossain <raselinfo52@gmail.com>
2021-07-27 23:44:31 +0600

    update

---
+++ Class 109 (Javascript)/index.js
@@ -0,0 +1,111 @@
+// let myObj={
+//     name:"Rasel",
+//     age:20,
+//     like:["movie","playing","dancing"],
+//     sub:{
+//         fistSub:"Bangla",
+//         secondSub:"English"
+//     }
+
+// }
+// myObj.car=["TVS","Toyota"]
+// // console.log(myObj.like[2])
+// console.log(myObj)
+
+// let arr=Array("1","2")
+// let myObj=new Object()
+// myObj.name="Rasel";
+// myObj.age=20
+
+// console.log(myObj)
+
+// let person={
+//     name:"Rasel",
+//     age:20,
+//     like:["movie","playing","dancing"],
+//     sub:{
+//         fistSub:"Bangla",
+//         secondSub:"English"
+//     }
+
+// }
+
+// console.log(person.name)
+// console.log(person['name'])
+// console.log(person['like'])
+// let x="like";
+// console.log(person[x])
+// let name=prompt("Enter your name")
+// let person={
+//     name: name
+// }
+// console.log(person.name)
+// let person={
+//     name:"Rasel",
+//     age:20,
+//     like:["movie","playing","dancing"],
+//     sub:{
+//         fistSub:"Bangla",
+//         secondSub:"English"
+//     }
+
+// }
+// delete person.sub
+// console.log(person)
+// let arr1 = [1, 2, 3, 4]
+// let arr2 = [1, 2, 3, 4]
+
+// console.log(arr1[0]===arr2[0])
+
+// const obj1={
+//     name:"rasel"
+// }
+// const obj2={
+//     name:"rasel"
+// }
+// console.log(obj1.name===obj2.name)
+// console.log(typeof obj1)
+// console.log(JSON.stringify(obj1) === JSON.stringify(obj2))
+
+
+// let person={
+//     name:"Rasel",
+//     age:20,
+//     like:["movie","playing","dancing"],
+//     sub:{
+//         fistSub:"Bangla",
+//         secondSub:"English"
+//     }
+// }
+// console.log("age" in person)
+// console.log(person['name'])
+// for (let i in person){
+//     // console.log(i)
+//     console.log(i +" "+person[i]);    
+// }
+// console.log(Object.keys(person));
+// console.log(Object.values(person))
+// console.log(Object.entries(person))
+// const obj2={
+//     ...person
+// }
+// const obj2={
+//     ...person
+// }
+// obj2.x=50
+// let obj2=Object.assign({},person)
+// console.log(person);
+
+// console.log(obj2);
+
+
+// Function
+// Input
+// Relationship/processing
+// Output
+
+
+// function myFunc(){
+//     console.log("I am Function");    
+// }
+// myFunc()
---
+++ Class 110 (Javascript)/index.js
@@ -0,0 +1,85 @@
+// function functionName(a,b,c){
+//     functionBody
+// }
+
+
+// functionName()
+
+
+
+// for(let i=1;i<=10;i++){
+//     console.log("Nihid Hasan")
+// }
+// console.log("For Loop is end")
+// console.log(5+5)
+// var arr=[1,2,3]
+// for (let i = 1; i <= 10; i++) {
+//     console.log("Nihid Hasan")
+// }
+// console.log("For Loop is end")
+// console.log(5 + 5)
+// var arr = [1, 2, 3]
+// for (let i = 1; i <= 10; i++) {
+//     console.log("Rasel Hasan")
+// }
+
+// function printMyName(name){
+//     for(let i=1;i<=10;i++){
+//         console.log(name)
+//     }
+// }
+// printMyName()
+// console.log("For Loop is end")
+// console.log(5 + 5)
+// var arr = [1, 2, 3]
+// printMyName()
+// console.log("For Loop is end")
+// console.log(5 + 5)
+// var arr = [1, 2, 3]
+// printMyName("Rasel Hossain")
+
+
+// function sum(num1,num2){
+//     let result =num1+num2
+//     console.log(result)
+// }
+// sum(20,30)
+// sum(1,1)
+// function mul(num1,num2){
+//     console.log(num1*num2)
+// }
+// mul(5,5)
+// function add(a,b){
+//     console.log(a+b)
+// }
+
+// let num1=30;
+// let num2=50;
+// add(num1, num2)
+
+// let arr=[1,2,3,5,6,7,8]
+// // console.log(arr.length)
+// function arrCount(arr){
+//     let count=0;
+//     for(let i=0;i<arr.length;i++){
+//         count++;
+//     }
+//     console.log(count)
+// }
+// arrCount(arr)
+// arr.push(5,6,7)
+// arrCount(arr)
+
+var arr1 = [1, 2, 3, 4, 5, 6]
+var arr2 = [1, 62, 30, 4, 50, 6]
+var arr3 = [1, 2, 83, 40, 5, 6]
+function arrayPrint(arr){
+    let str=""
+    for(let i=0;i<=(arr.length-1);i++){
+       str+=`${arr[i]}`
+    }
+    console.log(str)
+    
+}
+arrayPrint(arr3)
+
