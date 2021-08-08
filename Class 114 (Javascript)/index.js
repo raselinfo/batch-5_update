eede0b07546f3a0ba7dbc34a7b4c3dcc38a71a82
Rasel Hossain <raselinfo52@gmail.com>
2021-08-08 22:56:09 +0600

    update

---
+++ Class 114 (Javascript)/index.js
@@ -0,0 +1,64 @@
+// const test=function(){
+//     let a=50;
+//     const test2=function(){
+//         return a;
+//     }
+//     return test2
+// }
+
+// console.dir(test())
+
+// function sample(a,b){
+//     let c=a+b; //7
+//     let d=a-b; //3
+//     return sum(c,d)   
+   
+// }
+// console.log(sample(5,2))
+// function sum(num1,num2){
+//     return num1+num2;
+// }
+
+// function sample(a,b,cb){
+//     let c=a+b; //25
+//     let d=a-b; //15
+//     let result=cb(c,d)
+//     return result;
+// }
+
+
+
+// console.log(sample(10, 5, sub))
+
+// console.log(sample(5, 2, sum))
+
+// console.log(sample(20,5,function(num1,num2){
+//     return num1*num2
+// }))
+// let div=sample(50,5,function(num1,num2){
+//     return num1/num2
+// })
+// console.log(div)
+
+// function sub(num1,num2){
+//     return num1-num2;
+// }
+// function sum(num1,num2){
+//     return num1+num2;
+// }
+
+// setTimeout(function(){
+//     console.log("Hi")
+// },1000)
+// function test(string){
+//     function sample(text){
+//         return text
+//     }
+//     setTimeout(function(){
+//         sample(string)
+//     },2000)
+
+//    return sample
+// }
+
+// console.log(test("Hello")())
