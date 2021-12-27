9c67b39f7e4f886ae24edd6c174e86dc8f0d1aff
Rasel Hossain <raselinfo52@gmail.com>
2021-12-27 21:05:49 +0600

    update

---
+++ Class 139 (Es6)/index.js
@@ -0,0 +1,44 @@
+// let numberSet = new Set([1, 2, 8, 3])
+// numberSet.add("9")
+// numberSet.clear()
+// numberSet.delete(3)
+// console.log(numberSet.has(8))
+// console.log(numberSet)
+// for(let i=0;i<numberSet.size;i++){
+//     console.log(numberSet.values())
+// }
+
+// let iterator=numberSet.values()
+// console.log(iterator.next())
+// console.log(iterator.next())
+// console.log(iterator.next())
+// console.log(iterator.next())
+// console.log(iterator.next())
+// console.log(iterator.next())
+
+// for(let value of numberSet){
+//     console.log(value)
+// }
+// numberSet.forEach((item,_,arr)=>{
+//     console.log(arr)
+// })
+
+
+// let numberMap=new Map([
+//     // {a:1},
+//     ["a",1],
+//     ["b",2]
+// ])
+// numberMap.delete("a")
+// numberMap.clear()
+// numberMap.has("b")
+// console.log(numberMap.has("c"))
+// console.log(numberMap)
+// let iterator=numberMap.values()
+// console.log(iterator.next())
+// console.log(iterator.next())
+// console.log(iterator.next())
+// let a = {a:20},b={b:20}
+// let numberSet = new WeakSet([a,b])
+// a=null
+// console.log(numberSet)
