3319afc65fd0c06c46e76ffc3dd05f50244ab225
Rasel Hossain <raselinfo52@gmail.com>
2022-01-24 22:26:23 +0600

    Asynchronous javascript

---
+++ Class 142 (Es6)/index.js
@@ -0,0 +1,52 @@
+// console.log("I am line one")
+// console.log("I am line two")
+// setTimeout(() => {
+//     console.log("I am line three")
+// }, 2000)
+// console.log("I am line four")
+// setTimeout(() => {
+//     console.log("I am line Five")
+// }, 1000)
+
+// setTimeout(() => {
+//     console.log("I am line Six")
+// }, 500)
+
+// console.log("I am line Seven")
+
+
+// function sum(number) {
+//     let result;
+
+//     setTimeout(() => {
+//         result = number
+//         console.log("I AM in set time out")
+//     }, 0)
+//     console.log("Result ",result)
+//     return result
+// }
+// let output=sum(50)
+// console.log(output)
+
+
+
+// console.log("I am line one")
+// console.log("I am line two")
+// setTimeout(() => {
+//     console.log("I am line three")
+// }, 2000)
+// console.log("I am line four")
+// setTimeout(() => {
+//     console.log("I am line Five")
+// }, 1000)
+
+// setTimeout(() => {
+//     console.log("I am line Six")
+// }, 500)
+
+// console.log("I am line Seven")
+
+// * HOME WORK
+// API
+// JSON
+// AJAX
